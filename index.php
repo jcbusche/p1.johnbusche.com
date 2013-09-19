1cbbe92ed9d9f53f9ddb7a93d440e494969c9a20
jcbusche <jcbusche@gmail.com>
2013-09-19 00:59:53 -0400

    Started bio part of project

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <html>
-Welcome to P1.
+<header><title>John Busche</title></header>
+<body>
+<p>
+	Hi, my name is John. I'm 23 years old and currently taking time off of my undergraduate 
+	degree to reconsider my major and general life trajectory.  I'm taking this class because 
+	I'm contemplating switching from mechanical engineering to computer science and thought
+	it would be a good idea to get my feet wet first.
+</p>
+<p>
+	I'm submitting this late because I also have a part time job at a startup in Cambridge 
+	that has kept me very busy the past couple weeks and made it difficult for me to find 
+	the time to sit down and go through all of the setup for this project.  This shouldn't happen
+	again.
+</p> 
+</body>
 </html>
